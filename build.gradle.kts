54613e418540aad7f0e5485b1b4d503d0d2c397c
seiko <seiko_des@outlook.com>
2022-05-26 15:12:00 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application").version("7.2.0").apply(false)
+    id("com.android.library").version("7.2.0").apply(false)
+    kotlin("android").version("1.6.21").apply(false)
+    id("org.jetbrains.compose").version("1.2.0-alpha01-dev686").apply(false)
+    // id 'com.android.application' version '7.2.0' apply false
+    // id 'com.android.library' version '7.2.0' apply false
+    // id 'org.jetbrains.kotlin.android' version '1.5.31' apply false
+}
