6d4b1b8243891ae4e7fac8236a626d13e4784e49
seiko <seiko_des@outlook.com>
2022-07-19 11:18:48 +0800

    version to 1.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ allprojects {
     }
 
     group = "io.github.qdsfdhvh"
-    version = "1.0.3"
+    version = "1.0.4"
 
     plugins.withId("com.vanniktech.maven.publish.base") {
         @Suppress("UnstableApiUsage")
